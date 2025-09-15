94ea47d7fb4eec40759dc4cddb0b6109c61845d1
NguyenTrungKien <51208515+SubaruLenz@users.noreply.github.com>
2025-09-15 07:21:53 +0000

    167 completed

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -3,16 +3,5 @@ class Solution:
         if len(nums) < 3:
             return []
         else:
-            result = []
-            sum = []
-            nums.sort()
-            seen = set()
-            for i in range(len(nums)-2):
-                for j in range(i+1, len(nums)-1):
-                    filtered = nums[j+1:]
-                    if -(nums[i] + nums[j]) in filtered:
-                        sum = [nums[i], nums[j], -(nums[i]+nums[j])]
-                        if tuple(sum) not in seen:
-                            result.append(sum)
-                            seen.add(tuple(sum))
-            return result
+            sorted_nums = nums.sort()
+            
